569c2fe6bd8b6be7f738bcbad6d411b87484ec8b
JT <foosyou@gmail.com>
2020-07-13 20:45:01 -0500

    host vars

---
+++ nr-host-data.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python3
+
+"""
+This script is used to run a command on network devices with Nornir
+"""
+
+from nornir import InitNornir
+from nornir.plugins.tasks.networking import netmiko_send_command
+from nornir.plugins.functions.text import print_result
+
+
+def run_command(task):
+    # command to be run
+    cmd = "show ip interface brief | i Loop"
+    # send command to device
+    output = task.run(
+        task=netmiko_send_command, 
+        command_string=cmd
+    )
+
+    task.host["version"] = output.result
+
+def print_version(task):
+    print(task.host)
+    print(task.host["version"])
+    print()
+
+def main():
+    # initialize The Norn
+    nr = InitNornir()
+    # run The Norn to run command
+    nr.run(task=run_command)
+    # print results for each host
+    nr.run(task=print_version)
+   
+
+if __name__ == "__main__":
+    main()
